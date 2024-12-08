59282626110b7dea7a6f3de10709e30983f7841c
VegitoSama <kushagra.cha@gmail.com>
2024-12-08 07:11:13 +0530

    first commmit

---
+++ _next/static/chunks/app/layout-f4058c1a80d7fa26.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{1836:(e,s,a)=>{Promise.resolve().then(a.t.bind(a,8563,23)),Promise.resolve().then(a.t.bind(a,9840,23))},8563:()=>{},9840:e=>{e.exports={style:{fontFamily:"'Inter', 'Inter Fallback'",fontStyle:"normal"},className:"__className_a685e7"}}},e=>{var s=s=>e(e.s=s);e.O(0,[774,441,517,358],()=>s(1836)),_N_E=e.O()}]);
